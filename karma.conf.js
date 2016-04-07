11d222a84a49960c4824490aff806358fa81beff
Cameron Gregory <cameron@see-out.com>
2016-04-07 13:41:25 +1000

    add grunt and karma

---
+++ karma.conf.js
@@ -0,0 +1,14 @@
+
+module.exports = function( config ) {
+
+	config.set( {
+		files: [
+			"node_modules/jquery/dist/jquery.js",
+			"dist/jquery.touchcircle.min.js",
+			"test/setup.js",
+			"test/spec/*"
+		],
+		frameworks: [ "qunit" ],
+		autoWatch: true
+	} );
+};
